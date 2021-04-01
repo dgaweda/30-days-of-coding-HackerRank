1c046cb8a96c8b8a8a03900dc2a34dc012a732c3
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-04-01 12:38:08 +0200

    Day 8 HackerRank Task

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Solution
+{
+    static void Main()
+    {
+        int input = Convert.ToInt32(Console.ReadLine());
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        for (int i = 0; i < input; i++)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            phoneBook.Add(data[0], data[1]);
+        }
+
+        string name;
+        while((name = Console.ReadLine()) != null)
+        {
+            if (phoneBook.ContainsKey(name))
+                Console.WriteLine(name + "=" + phoneBook[name]);
+            else
+                Console.WriteLine("Not found");
+        }
+    }
+}
---
+++ Day8/Day8/task.md
@@ -0,0 +1,2 @@
+- Day 8 - HackerRank Task -
+https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
