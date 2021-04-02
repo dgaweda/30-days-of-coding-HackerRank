01ca0c58b1274a0a35998335facc1ff266f4c090
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-04-02 13:21:12 +0200

    finished: Day 9/30

---
+++ Day9/day9/day9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the factorial function below.
+    static int factorial(int n)
+    {
+        if (n == 1)
+            return 1; // stop point
+        else
+            return n * factorial(n - 1); // recursion
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine()); // convert to int
+
+        int result = factorial(n); // using method
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Day9/day9/day9/day9.md
@@ -0,0 +1,1 @@
+- Day 9 - recursion - https://www.hackerrank.com/challenges/30-recursion/problem
