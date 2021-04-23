9b452ce12cdb1803a8f4a957fd213e589ba25378
darek <realistic115@wp.pl>
2021-04-23 11:57:52 +0200

    xyz

---
+++ Day20/Day20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day20
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] temp_arr = Console.ReadLine().Split(' ');
+            int[] values = Array.ConvertAll(temp_arr, Int32.Parse);
+            values = BubbleSort(values);
+            
+            foreach(var i in values)
+                Console.Write(i);
+        }
+
+        public static int[] BubbleSort(int[] arr)
+        {
+            var temp = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                for (var j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
