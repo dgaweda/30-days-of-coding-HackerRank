927353c6bf677fbea1f598320d504f89cf2a2293
darek <realistic115@wp.pl>
2021-04-24 21:14:59 +0200

    Finished Day 21

---
+++ Day21/Day21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Day21
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+		
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+		
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+        }
+
+        private static void PrintArray<T>(T[] Array)
+        {
+            foreach(var elem in Array)
+                Console.WriteLine(elem);
+        }
+    }
+}
---
+++ Day21/Day21/task.md
@@ -0,0 +1,2 @@
+- Day 21 - Generics method <br />
+  https://www.hackerrank.com/challenges/30-generics/problem
