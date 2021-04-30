d2a4c7a1a3e3fdd9451ef61e496f5cd8e212f3fd
darek <realistic115@wp.pl>
2021-04-30 11:06:18 +0200

    Fin

---
+++ Day23/Day23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Day23;
+
+class Solution
+{
+    static Queue<Node> queue = new Queue<Node>();
+    static void levelOrder(Node root)
+    {
+        if (root != null)
+        {
+            Console.Write(root.data + " ");
+            if (root.left != null)
+            {
+                queue.Enqueue(root.left);
+            }
+
+            if (root.right != null)
+            {
+                queue.Enqueue(root.right);
+            }
+
+            if (queue.Count != 0)
+            {
+                levelOrder(queue.Dequeue());
+            }
+        }
+    }
+    
+    static void Main(String[] args)
+    { 
+        Node root = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            root = Actions.insert(root, data);
+        }
+        levelOrder(root);
+    }
+}
