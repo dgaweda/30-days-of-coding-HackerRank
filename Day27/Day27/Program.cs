da33a5eda7715f8e34f133adb2613b69a9b56bcc
darek <realistic115@wp.pl>
2021-05-05 19:12:34 +0200

    day27

---
+++ Day27/Day27/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            string firstName = firstMultipleInput[0];
+            string emailID = firstMultipleInput[1];
+            var checkedName = CheckName(firstName).ToString();
+            var checkedEmail = CheckEmail(emailID).ToString();
+            
+            if (firstName.Equals(checkedName) && emailID.Equals(checkedEmail))
+            {
+                names.Add(firstName);
+            }
+        }
+        
+        names.Sort();
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    private static object CheckName(string name)
+    {
+        string regExName = @"([a-z])\w+";
+
+        return Regex.Match(name, regExName);
+    }
+
+    private static object CheckEmail(string email)
+    {
+        string regExEmail = @"([a-z])\w+(.)+([a-z])(@)+(gmail)+(.)+(com)";
+        
+        return Regex.Match(email, regExEmail);
+    }
+}
